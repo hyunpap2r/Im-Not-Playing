6ac03e31b921f8ea6221f29fa9aefb4f1a4f3003
hyunpaper <hyunpaper5170@naver.com>
2024-05-23 23:11:45 +0900

    Add Google Assistant

---
+++ GoogleAssistantClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Im_Not_Playing
+{
+    internal class GoogleAssistantClient
+    {
+        private readonly HttpClient _client;
+        private readonly string _accessToken;
+        private readonly string _apiEndpoint;
+
+        public GoogleAssistantClient(string accessToken, string apiEndpoint)
+        {
+            _client = new HttpClient();
+            _accessToken = accessToken;
+            _apiEndpoint = apiEndpoint;
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        }
+
+
+        public async Task<string> SendQueryAsync(string query)
+        {
+            var requestBody = new { query = query };
+            var content = new StringContent(JsonConvert.SerializeObject(requestBody), System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync(_apiEndpoint, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+        }
+
+
+    }
+}
